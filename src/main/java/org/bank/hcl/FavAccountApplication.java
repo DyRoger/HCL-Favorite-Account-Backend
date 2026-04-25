7d6c2ab4ec886c9e25e55d3e14e96258609b26be
Kaustabh Adak <kaustabhadak35@gmail.com>
2026-04-25 11:42:44 +0530

    initial commit

---
+++ src/main/java/org/bank/hcl/FavAccountApplication.java
@@ -0,0 +1,13 @@
+package org.bank.hcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bank/hcl/FavAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bank.hcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
